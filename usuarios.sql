80aeebd624b3f320a35d01fff2f0481d206e1f22
juanprzzz <juan.perezresa@gmail.com>
2025-01-10 18:45:24 +0100

    Modificado usuarios.sql. Ahora hay grupos de usuarios, y a los usuarios se les asigna un grupo, ver comentario de usuarios.sql para eliminar los usuarios anteriores y poner los nuevos

--- usuarios.sql
+++ usuarios.sql
@@ -1,16 +1,42 @@
-/* Creando usuarios */
+/* Creando grupos de usuarios */
+
+CREATE ROLE admins;
+CREATE ROLE gestores;
+CREATE ROLE clientes;
+CREATE ROLE invitados;
+
+/*Asignación de permisos*/
+
+GRANT ALL ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO admins;
+GRANT INSERT, UPDATE, DELETE, SELECT ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO gestores;
+
+GRANT SELECT ON tiene, desea TO clientes;
+GRANT INSERT ON tiene, desea TO clientes;
+
+GRANT SELECT ON disco, cancion TO invitados;
+
+/*Creando usuarios y asignándoles un role*/
 
 CREATE USER admin WITH PASSWORD 'admin';
 CREATE USER gestor WITH PASSWORD 'gestor';
 CREATE USER cliente WITH PASSWORD 'cliente';
 CREATE USER invitado WITH PASSWORD 'invitado';
 
-/*Asignación de permisos*/
+GRANT admins TO admin;
+GRANT gestores TO gestor;
+GRANT clientes TO cliente;
+GRANT invitados TO invitado;
 
-GRANT ALL ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO admin;
-GRANT INSERT, UPDATE, DELETE, SELECT ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO gestor;
 
-GRANT SELECT ON tiene, desea TO cliente;
-GRANT INSERT ON tiene, desea TO cliente;
+/*
+Para quitar los usuarios anteriores hay que llevar a cabo los siguientes comandos, primero hay que quitar todos los permisos, y luego quitar los usuarios, una vez hecho eso, volver a ejecutar el .sql:
+    Para admin:
+    REVOKE ALL PRIVILEGES ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario FROM admin; DROP USER admin;
+    Para gestor:
+    REVOKE ALL PRIVILEGES ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario FROM gestor; DROP USER gestor;
+    Para cliente:
+    REVOKE ALL PRIVILEGES ON tiene, desea FROM cliente; DROP USER cliente;
+    Para invitado:
+    REVOKE ALL PRIVILEGES ON disco, cancion FROM invitado; DROP USER invitado;
+*/
 
-GRANT SELECT ON disco, cancion TO invitado;
