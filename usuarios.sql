0ff80508c9f2f1058dcd58fefdc501a0d8ce3f19
juanprzzz <juan.perezresa@gmail.com>
2025-01-12 19:12:54 +0100

    ...

--- usuarios.sql
+++ usuarios.sql
@@ -1,14 +1,14 @@
 /* Creando grupos de usuarios */
 
-CREATE ROLE admins WITH SUPERUSER;
+CREATE ROLE admins;
 CREATE ROLE gestores;
 CREATE ROLE clientes;
 CREATE ROLE invitados;
 
 /*Asignación de permisos*/
 
---GRANT ALL ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO admins;
---GRANT CREATE ON DATABASE pl3 TO admins;
+GRANT ALL ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO admins;
+GRANT CREATE ON DATABASE pl3 TO admins;
 GRANT INSERT, UPDATE, DELETE, SELECT ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO gestores;
 
 GRANT SELECT ON tiene, desea TO clientes;
