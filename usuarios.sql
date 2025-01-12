9cc34326b61598a9aa65a326fb30a0824c0e58c1
juanprzzz <juan.perezresa@gmail.com>
2025-01-12 19:09:22 +0100

    Modificado admin SUPERUSER

--- usuarios.sql
+++ usuarios.sql
@@ -1,13 +1,14 @@
 /* Creando grupos de usuarios */
 
-CREATE ROLE admins;
+CREATE ROLE admins WITH SUPERUSER;
 CREATE ROLE gestores;
 CREATE ROLE clientes;
 CREATE ROLE invitados;
 
 /*Asignación de permisos*/
 
-GRANT ALL ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO admins;
+--GRANT ALL ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO admins;
+--GRANT CREATE ON DATABASE pl3 TO admins;
 GRANT INSERT, UPDATE, DELETE, SELECT ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario TO gestores;
 
 GRANT SELECT ON tiene, desea TO clientes;
@@ -31,12 +32,12 @@ GRANT invitados TO invitado;
 /*
 Para quitar los usuarios anteriores hay que llevar a cabo los siguientes comandos, primero hay que quitar todos los permisos, y luego quitar los usuarios, una vez hecho eso, volver a ejecutar el .sql:
     Para admin:
-    REVOKE ALL PRIVILEGES ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario FROM admin; DROP USER admin;
+    REVOKE ALL PRIVILEGES ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario FROM admin; DROP USER admin;DROP USER admins;
     Para gestor:
-    REVOKE ALL PRIVILEGES ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario FROM gestor; DROP USER gestor;
+    REVOKE ALL PRIVILEGES ON auditoria, cancion, desea, disco, edicion, genero, grupo, tiene, usuario FROM gestor; DROP USER gestor;DROP user gestores;
     Para cliente:
-    REVOKE ALL PRIVILEGES ON tiene, desea FROM cliente; DROP USER cliente;
+    REVOKE ALL PRIVILEGES ON tiene, desea FROM cliente; DROP USER cliente;DROP USER clientes;
     Para invitado:
-    REVOKE ALL PRIVILEGES ON disco, cancion FROM invitado; DROP USER invitado;
+    REVOKE ALL PRIVILEGES ON disco, cancion FROM invitado; DROP USER invitado;DROP USER invitados;
 */
 
